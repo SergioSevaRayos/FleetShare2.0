b2b1fc770717859693e9c6beb3a71ee0947b6b3a
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-11-16 19:42:56 +0100

    V2.0 Fleet Share

---
+++ logica.js
@@ -0,0 +1,5 @@
+ // Obtener el año actual
+ var currentYear = new Date().getFullYear();
+
+ // Mostrar el año actual en el elemento con el id "currentYear"
+ document.getElementById("currentYear").textContent = currentYear;
